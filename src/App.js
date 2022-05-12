1c54abb13fbb7c42ef90864f363ff3e5c75cfa73
Sake Swaraj <sakeswaraj18@gmail.com>
2022-05-12 16:09:07 +0530

    tailwind and other setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center text-blue-700">
+      react food delivery app
+    </div>
+  )
+}
+
+export default App
