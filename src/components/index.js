9ab50b1ec006fbfb63b2e677124909412625d992
Sake Swaraj <sakeswaraj18@gmail.com>
2022-05-18 09:38:12 +0530

    saving data to firebase

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export {default as Header} from "./Header/Header";
 export {default as MainContainer} from "./MainContainer/MainContainer";
 export {default as CreateItem} from "./CreateItem/CreateItem";
 export {default as HomeContainer} from "./HomeContainer/HomeContainer"
+export {default as Loader} from "./Loader"
